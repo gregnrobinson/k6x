e3557172712dfec5351492dfe020070aa8a41e4e
Greg Robinson <gregnrobinson@gmail.com>
2021-07-13 18:39:24 -0400

    cleanup

--- run.sh
+++ run.sh
@@ -5,18 +5,17 @@ set -o pipefail
 environment() {
   source settings
   echo "Setting up environment..."
-  yq eval '.substitutions._TEST_NAME              |= ''"'$TEST_NAME'"'              -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._IMG_DEST               |= ''"'$IMG_DEST'"'               -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._LOCATION               |= ''"'$LOCATION'"'               -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._K6_VUS                 |= ''"'$K6_VUS'"'                 -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._K6_DURATION            |= ''"'$K6_DURATION'"'            -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._BQ_DATASET_NAME        |= ''"'$BQ_DATASET_NAME'"'        -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._BQ_DATASET_FORMAT      |= ''"'$BQ_DATASET_FORMAT'"'      -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._BQ_DATASET_DESCRIPTION |= ''"'$BQ_DATASET_DESCRIPTION'"' -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._BQ_TABLE_NAME          |= ''"'$BQ_TABLE_NAME'"'          -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._GCS_FILE_NAME          |= ''"'$GCS_FILE_NAME'"'          -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._GCS_BUCKET_NAME        |= ''"'$GCS_BUCKET_NAME'"'        -i ./config/cloudbuild_local.yaml
-  yq eval '.substitutions._GCP_SA_EMAIL           |= ''"'$GCP_SA_EMAIL'"'           -i ./config/cloudbuild_local.yaml
+  yq eval '.substitutions._TEST_NAME              |= ''"'$TEST_NAME'"'              -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._IMG_DEST               |= ''"'$IMG_DEST'"'               -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._LOCATION               |= ''"'$LOCATION'"'               -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._K6_VUS                 |= ''"'$K6_VUS'"'                 -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._K6_DURATION            |= ''"'$K6_DURATION'"'            -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._BQ_DATASET_NAME        |= ''"'$BQ_DATASET_NAME'"'        -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._BQ_DATASET_FORMAT      |= ''"'$BQ_DATASET_FORMAT'"'      -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._BQ_DATASET_DESCRIPTION |= ''"'$BQ_DATASET_DESCRIPTION'"' -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._BQ_TABLE_NAME          |= ''"'$BQ_TABLE_NAME'"'          -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._GCS_FILE_NAME          |= ''"'$GCS_FILE_NAME'"'          -i ./config/cloudbuild.yaml
+  yq eval '.substitutions._GCS_BUCKET_NAME        |= ''"'$GCS_BUCKET_NAME'"'        -i ./config/cloudbuild.yaml
 }
 
 ddos-local(){
@@ -25,7 +24,7 @@ ddos-local(){
     rm -rf $TEST_PATH/workspace
 
     cloud-build-local \
-      --config=${TEST_PATH}/cloudbuild_local.yaml \
+      --config=${TEST_PATH}/cloudbuild.yaml \
       --write-workspace=$TEST_PATH \
       --dryrun=false $TEST_PATH
 }
